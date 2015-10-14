d17d4e8b0a17c0b713b25276e402d345fb310ea9
bilalyasar <bilalyasar45@gmail.com>
2015-10-14 20:47:44 +0300

    initial hello world web aplication module is created

---
+++ bounswe2015group7-registration/src/MainServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by bilal on 14/10/15.
+ */
+@WebServlet(name = "MainServlet")
+public class MainServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getOutputStream().println("Hello World!");
+
+    }
+}
---
+++ bounswe2015group7-registration/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: bilal
+  Date: 14/10/15
+  Time: 20:37
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>

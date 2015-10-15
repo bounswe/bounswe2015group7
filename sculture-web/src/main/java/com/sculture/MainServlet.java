4d778fb91cbe6e147ea87fb056142a6ee02a3caf
bilalyasar <bilalyasar45@gmail.com>
2015-10-15 19:08:12 +0300

    wip

--- sculture-web/src/main/java/com/sculture/MainServlet.java
+++ sculture-web/src/main/java/com/sculture/MainServlet.java
@@ -45,7 +45,7 @@ public class MainServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        request.getRequestDispatcher("/user.jsp?").forward(request, response);
+        doGet(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {

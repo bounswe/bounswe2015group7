f5a4187f9d986f94ca89ecd83e3ee59eb5b41bdb
bilalyasar <bilalyasar45@gmail.com>
2015-10-25 02:53:40 +0300

    added example rest api

---
+++ sculture-rest/src/main/java/hello/User.java
@@ -0,0 +1,17 @@
+package hello;
+
+/**
+ * Created by bilal on 25/10/15.
+ */
+public class User {
+
+    private final String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}

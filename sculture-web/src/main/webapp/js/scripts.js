f5428d2a0e2239f3541e0dac06de283021b23f1b
atakanarikan <arikan.atakan@yahoo.com>
2015-11-06 01:46:44 +0300

    Added a template for the homapage, consisting of login & registration pages(popups).

---
+++ sculture-web/src/main/webapp/js/scripts.js
@@ -0,0 +1,39 @@
+
+jQuery(document).ready(function() {
+	
+    /*
+        Fullscreen background
+    */
+   // $.backstretch("assets/img/backgrounds/1.jpg");
+    
+    /*
+	    Modals
+	*/
+	$('.launch-modal').on('click', function(e){
+		e.preventDefault();
+		$( '#' + $(this).data('modal-id') ).modal();
+	});
+    
+    /*
+        Form validation
+    */
+	$('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.login-form').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+    
+    
+});

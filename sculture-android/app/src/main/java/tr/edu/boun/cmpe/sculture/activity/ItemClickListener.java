d0adfab5dbf808509f8622c6f88917da4b5f1e2d
MrPaksoy <e.m.paksoy@gmail.com>
2015-11-10 12:16:44 +0200

    Search and retreive methods are almost implemented on Android

---
+++ sculture-android/app/src/main/java/tr/edu/boun/cmpe/sculture/activity/ItemClickListener.java
@@ -0,0 +1,10 @@
+package tr.edu.boun.cmpe.sculture.activity;
+
+import android.view.View;
+
+/**
+ * Created by MrPaksoy on 10/11/15.
+ */
+public interface ItemClickListener {
+    void onClick(View view, int position);
+}
--- sculture-android/app/src/main/java/tr/edu/boun/cmpe/sculture/activity/SearchActivity.java
+++ sculture-android/app/src/main/java/tr/edu/boun/cmpe/sculture/activity/SearchActivity.java
@@ -1,7 +1,10 @@
 package tr.edu.boun.cmpe.sculture.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,6 +25,12 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
     EditText searchText;
     TextView resultText;
     Button searchButton;
+    RecyclerView mRecyclerView;
+    private LinearLayoutManager mLayoutManager;
+    ArrayList<ArrayList<String>> myDataset = new ArrayList<ArrayList<String>>();
+    ArrayList<String> story = new ArrayList<String>();
+    private RecyclerViewAdaptor mRecyclerViewAdaptor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,12 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
         searchText = (EditText) findViewById(R.id.searchEditText);
         resultText = (TextView) findViewById(R.id.searchResultText);
         searchButton.setOnClickListener(this);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -55,15 +70,36 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
                     public void done(ArrayList<HashMap> list, ParseException e) {
                         if (e == null) {
                             String s = "";
-                            for (HashMap item : list)
-                                s += item.get("title") + "\n";
+                            myDataset.clear();
+                            for (HashMap item : list) {
 
+                                //s += item.get("title") + "\n";
+                                ArrayList <String> story = new ArrayList<String>();
+                                story.add((String) item.get("title"));
+                                //story.add("Tags: "/*(String) item.get("tags")*/);
+                                //story.add("Content: "+(String) item.get("content"));
+                                //ParseUser usr = (ParseUser)item.get("ownerId");
+                                //story.add("ownerId: "+(String)item.get("ownerId").toString());
+                                //story.add("created at: "+(String) item.get("createdAt"));
+                                //story.add("LastEditor ID: "+(String) item.get("lastEditorId").toString());
+                                story.add((String) item.get("updatedAt"));
+                                story.add((String) item.get("id"));
+                                myDataset.add(story);
+                            }
+                            mRecyclerViewAdaptor = new RecyclerViewAdaptor(myDataset , SearchActivity.this);
+
+                            mRecyclerView.setAdapter(mRecyclerViewAdaptor);
                             resultText.setText(s);
-
                         } else
                             Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
                     }
                 }
         );
+    }
+
+    public void sendToNextActivity(String id) {
+        Intent intent = new Intent(this, StoryShowActivity.class);
+        intent.putExtra("id", id);
+        startActivity(intent);
     }
 }

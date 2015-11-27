30186bce2439cad63dc2b3662ef0cebef32ed638
bilalyasar <bilalyasar45@gmail.com>
2015-11-28 01:38:58 +0200

    improved web and rest

---
+++ sculture-web/src/main/java/com/sculture/AddStory.java
@@ -0,0 +1,34 @@
+package com.sculture;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(name = "addstory")
+public class AddStory extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("isLoggedIn", false);
+        request.setAttribute("username", "");
+        if (request.getSession().getAttribute("username") != null) {
+            request.setAttribute("username", request.getSession().getAttribute("username"));
+            request.setAttribute("isLoggedIn", true);
+        }
+        request.getRequestDispatcher("/add_story.jsp").forward(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("isLoggedIn", false);
+        request.setAttribute("username", "");
+        if (request.getSession().getAttribute("username") != null) {
+            request.setAttribute("username", request.getSession().getAttribute("username"));
+            request.setAttribute("isLoggedIn", true);
+        }
+        request.getRequestDispatcher("/add_story.jsp").forward(request, response);
+    }
+
+}

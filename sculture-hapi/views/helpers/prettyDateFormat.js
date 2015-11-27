97a4e02e8ac1bc80e94b561e54cf3b46de6645a0
bilalyasar <bilalyasar45@gmail.com>
2015-11-28 01:42:48 +0200

    removed hapi

--- sculture-hapi/views/helpers/prettyDateFormat.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Atakan Arıkan on 10.11.2015.
- */
-var Handlebars = require("handlebars");
-Handlebars.registerHelper("prettyDateFormat", function(date) {
-    var myDate = new Date(date);
-    return myDate.toDateString();
-});

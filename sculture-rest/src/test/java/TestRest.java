c1dc5f854de9405f313b4e38ec0018e01845f745
bilalyasar <bilalyasar45@gmail.com>
2015-11-29 22:28:33 +0200

    test edit

--- sculture-rest/src/test/java/TestRest.java
+++ sculture-rest/src/test/java/TestRest.java
@@ -1,11 +1,12 @@
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONObject;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import sculture.Application;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by bilal on 29/11/15.
@@ -21,28 +22,23 @@ public class TestRest {
     }
 
     @Test
-    public void test() throws Exception {
+    public void testSample() throws Exception {
 
         HttpResponse<JsonNode> jsonResponse = null;
-        try {
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("username", "test-user");
+        jsonObject.put("password", "test-password");
+        jsonObject.put("email", "test-password@test.com");
 
 
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("username", "test-user");
-            jsonObject.put("password", "test-password");
-            jsonObject.put("email", "test-password@test.com");
-
-
-            JsonNode jsonNode = new JsonNode(jsonObject.toString());
-            jsonResponse = Unirest.post("http://52.28.216.93:9000/user/register")
-                    .header("Content-Type", "application/json")
-                    .body(jsonNode)
-                    .asJson();
-        } catch (UnirestException e) {
-            e.printStackTrace();
-        }
-        if (jsonResponse != null) {
-            System.out.println(jsonResponse.getBody());
-        }
+        JsonNode jsonNode = new JsonNode(jsonObject.toString());
+        jsonResponse = Unirest.post("http://52.28.216.93:9000/user/register")
+                .header("Content-Type", "application/json")
+                .body(jsonNode)
+                .asJson();
+        assertEquals(jsonResponse.getBody().getObject().getString("username"), "test-user");
+        assertEquals(jsonResponse.getBody().getObject().getString("password"), "test-password");
+        assertEquals(jsonResponse.getBody().getObject().getString("email"), "test-password@test.com");
     }
 }

7359b0428f2cbaaf14c0ca581266d760e6377046
MrPaksoy <e.m.paksoy@gmail.com>
2015-12-01 10:24:32 +0200

    [ANDROID] Fix bugs

--- sculture-android/app/src/main/java/tr/edu/boun/cmpe/sculture/Constants.java
+++ sculture-android/app/src/main/java/tr/edu/boun/cmpe/sculture/Constants.java
@@ -1,17 +1,33 @@
 package tr.edu.boun.cmpe.sculture;
 
 public class Constants {
+    //API URLS
+    private static final String API_BASE_URL = BuildConfig.API_BASE_URL;
+    public static final String API_USER_LOGIN = API_BASE_URL + "/user/login";
+    public static final String API_USER_REGISTER = API_BASE_URL + "/user/register";
+    public static final String API_STORY_GET = API_BASE_URL + "/story/get";
+    public static final String API_STORY_CREATE = API_BASE_URL + "/story/create";
+    public static final String API_SEARCH = API_BASE_URL + "/search";
+    public static final String API_COMMENT_LIST = API_BASE_URL + "/comment/list";
+    public static final String API_COMMENT_GET = API_BASE_URL + "/comment/get";
+
+
     //REQUEST TAGS
     public static final String REQUEST_TAG_LOGIN = "request_login";
     public static final String REQUEST_TAG_REGISTER = "request_register";
     public static final String REQUEST_TAG_SEARCH = "request_search";
     public static final String REQUEST_TAG_STORY_CREATE = "request_story_create";
     public static final String REQUEST_TAG_STORY_GET = "request_story_get";
+    public static final String REQUEST_TAG_COMMENT_LIST = "request_comment_list";
+    public static final String REQUEST_TAG_COMMENT_GET = "request_comment_get";
+
+
     //PREFS
     public static final String PREFS_NAME = "MyPrefsFile";
     public static final String PREF_EMAIL = "email";
     public static final String PREF_USERNAME = "username";
     public static final String PREF_ACCESS_TOKEN = "token";
+
     //JSON FIELDS
     public static final String FIELD_USERNAME = "username";
     public static final String FIELD_EMAIL = "email";
@@ -27,22 +43,13 @@ public class Constants {
     public static final String FIELD_UPDATE_DATE = "update_date";
     public static final String FIELD_LAST_EDITOR = "last_editor";
     public static final String FIELD_TAGS = "tags";
-    public static final String FIELD_POSITIVE_VOTE = "positive_vote";
+    public static final String FIELD_POSITIVE_VODE = "positive_vote";
     public static final String FIELD_NEGATIVE_VOTE = "negative_vote";
     public static final String FIELD_REPORT_COUNT = "report_count";
     public static final String FIELD_OWNER = "owner";
     public static final String FIELD_CONTENT = "content";
-    //INTENT BUNDLE KEYS
-    public static final String BUNDLE_STORY_ID = "story_id";
-    public static final String BUNDLE_IS_EDIT = "is_edit";
+
+
     //HEADER FIELDS
     public static final String HEADER_ACCESS_TOKEN = "access-token";
-    //API URLS
-    private static final String API_BASE_URL = BuildConfig.API_BASE_URL;
-    public static final String API_USER_LOGIN = API_BASE_URL + "/user/login";
-    public static final String API_USER_REGISTER = API_BASE_URL + "/user/register";
-    public static final String API_STORY_GET = API_BASE_URL + "/story/get";
-    public static final String API_STORY_CREATE = API_BASE_URL + "/story/create";
-    public static final String API_STORY_EDIT = API_BASE_URL + "/story/edit";
-    public static final String API_SEARCH = API_BASE_URL + "/search";
 }
--- sculture-android/app/src/main/java/tr/edu/boun/cmpe/sculture/activity/StoryShowActivity.java
+++ sculture-android/app/src/main/java/tr/edu/boun/cmpe/sculture/activity/StoryShowActivity.java
@@ -2,6 +2,8 @@ package tr.edu.boun.cmpe.sculture.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
 import com.android.volley.Response;
@@ -12,9 +14,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import tr.edu.boun.cmpe.sculture.R;
+import tr.edu.boun.cmpe.sculture.adapter.RecyclerViewAdaptor_Comments;
 
+import static tr.edu.boun.cmpe.sculture.Constants.API_COMMENT_GET;
 import static tr.edu.boun.cmpe.sculture.Constants.API_STORY_GET;
-import static tr.edu.boun.cmpe.sculture.Constants.BUNDLE_STORY_ID;
 import static tr.edu.boun.cmpe.sculture.Constants.FIELD_CONTENT;
 import static tr.edu.boun.cmpe.sculture.Constants.FIELD_CREATION_DATE;
 import static tr.edu.boun.cmpe.sculture.Constants.FIELD_ID;
@@ -24,25 +27,32 @@ import static tr.edu.boun.cmpe.sculture.Constants.FIELD_TAGS;
 import static tr.edu.boun.cmpe.sculture.Constants.FIELD_TITLE;
 import static tr.edu.boun.cmpe.sculture.Constants.FIELD_UPDATE_DATE;
 import static tr.edu.boun.cmpe.sculture.Constants.FIELD_USERNAME;
+import static tr.edu.boun.cmpe.sculture.Constants.REQUEST_TAG_COMMENT_GET;
 import static tr.edu.boun.cmpe.sculture.Constants.REQUEST_TAG_STORY_GET;
 import static tr.edu.boun.cmpe.sculture.Utils.addRequest;
 import static tr.edu.boun.cmpe.sculture.Utils.timestampToPrettyString;
 
 public class StoryShowActivity extends AppCompatActivity {
 
-    private TextView title;
-    private TextView content;
-    private TextView tags;
-    private TextView storyOwner;
-    private TextView createdAt;
-    private TextView lastEditor;
-    private TextView lastUpdatedAt;
+    private static final int SIZE = 10;
+    private int PAGE = 1;
+    TextView title;
+    TextView content;
+    TextView tags;
+    TextView storyOwner;
+    TextView createdAt;
+    TextView lastEditor;
+    TextView lastUpdatedAt;
+    RecyclerView rcw;
+    LinearLayoutManager mLayoutManager;
+    RecyclerViewAdaptor_Comments adaptor;
+    Bundle bundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story_show);
-        Bundle bundle = getIntent().getExtras();
+        bundle = getIntent().getExtras();
         title = (TextView) findViewById(R.id.story_title);
         content = (TextView) findViewById(R.id.storyContent);
         tags = (TextView) findViewById(R.id.storyTags);
@@ -50,13 +60,66 @@ public class StoryShowActivity extends AppCompatActivity {
         createdAt = (TextView) findViewById(R.id.storyCreatedAt);
         lastEditor = (TextView) findViewById(R.id.storyLastEditor);
         lastUpdatedAt = (TextView) findViewById(R.id.story_update_date);
-        long story_id = bundle.getLong(BUNDLE_STORY_ID, 0);
-        getStory(story_id);
+        if (bundle.getLong("id") != 0) {
+            getStory(bundle.getLong("id"));
+        }
+
+        rcw = (RecyclerView) findViewById(R.id.comment_recycler);
+        rcw.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        adaptor = new RecyclerViewAdaptor_Comments(this);
+        rcw.setLayoutManager(mLayoutManager);
+        rcw.setAdapter(adaptor);
+        rcw.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                load_comment();
+            }
+        });
 
     }
 
+    private void load_comment() {
+        final JSONObject requestBody = new JSONObject();
+        try {
+            if (bundle.getLong("id") != 0) {
+                requestBody.put("story_id", bundle.getLong("id"));
+               // requestBody.put(FIELD_SIZE, SIZE);
+               // requestBody.put(FIELD_PAGE, PAGE);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
-    private void getStory(long id) {
+        int totalItemCount = mLayoutManager.getItemCount();
+        int lastVisibleIndex = mLayoutManager.findLastVisibleItemPosition();
+        boolean loadMore = lastVisibleIndex == totalItemCount - 1;
+
+        addRequest(API_COMMENT_GET, requestBody, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                JSONArray array;
+                try {
+                    array = response.getJSONArray("comment");
+                    for (int i = 0; i < array.length(); i++)
+                        adaptor.addElement(array.getJSONObject(i));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }, REQUEST_TAG_COMMENT_GET);
+    }
+
+
+    public void getStory(long id) {
 
         // TODO get story request
 
@@ -75,10 +138,10 @@ public class StoryShowActivity extends AppCompatActivity {
                             title.setText(response.getString(FIELD_TITLE));
                             content.setText(response.getString(FIELD_CONTENT));
                             JSONArray tagsAr = response.getJSONArray(FIELD_TAGS);
-                            String tag_string = "";
+                            String tagstring = "";
                             for (int i = 0; i < tagsAr.length(); i++)
-                                tag_string += tagsAr.get(i) + ", ";
-                            tags.setText(tag_string);
+                                tagstring += tagsAr.get(i) + ", ";
+                            tags.setText(tagstring);
                             storyOwner.setText(response.getJSONObject(FIELD_OWNER).getString(FIELD_USERNAME));
                             lastEditor.setText(response.getJSONObject(FIELD_LAST_EDITOR).getString(FIELD_USERNAME));
                             createdAt.setText(timestampToPrettyString(response.getLong(FIELD_CREATION_DATE)));

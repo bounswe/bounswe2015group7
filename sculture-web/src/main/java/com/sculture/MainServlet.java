17ebae31fbb69dae5c1bc9fbc235c2f7b877e2e3
bilalyasar <bilalyasar45@gmail.com>
2015-12-08 02:29:29 +0200

    wip

--- sculture-web/src/main/java/com/sculture/MainServlet.java
+++ sculture-web/src/main/java/com/sculture/MainServlet.java
@@ -1,11 +1,19 @@
 package com.sculture;
 
+import com.google.gson.Gson;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.sculture.helpers.Story;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by bilal on 14/10/15.
@@ -36,12 +44,33 @@ public class MainServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Story story;
         request.setAttribute("isLoggedIn", false);
         request.setAttribute("username", "");
         if (request.getSession().getAttribute("username") != null) {
             request.setAttribute("username", request.getSession().getAttribute("username"));
             request.setAttribute("isLoggedIn", true);
         }
+
+        HttpResponse<JsonNode> jsonResponse = null;
+        try {
+            jsonResponse = Unirest.post("http://52.28.216.93:9000/search/all")
+                    .header("Content-Type", "application/json")
+                    .asJson();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<Story> stories = new ArrayList<Story>();
+        if (jsonResponse != null) {
+            for (int i = 0; i < jsonResponse.getBody().getArray().length(); i++) {
+                Object object = jsonResponse.getBody().getArray().get(i);
+                Gson gson = new Gson();
+                story = gson.fromJson(object.toString(), Story.class);
+                stories.add(story);
+            }
+        }
+        request.setAttribute("topStory",stories.get(0));
         request.getRequestDispatcher("/frontend_homepage.jsp").forward(request, response);
     }
 

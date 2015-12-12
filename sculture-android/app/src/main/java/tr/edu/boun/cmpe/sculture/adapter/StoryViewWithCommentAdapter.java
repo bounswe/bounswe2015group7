ec868afc01795282becf81a677f03c1ec89a3d0e
MrPaksoy <e.m.paksoy@gmail.com>
2015-12-12 22:08:56 +0200

    clickable tags

--- sculture-android/app/src/main/java/tr/edu/boun/cmpe/sculture/adapter/StoryViewWithCommentAdapter.java
+++ sculture-android/app/src/main/java/tr/edu/boun/cmpe/sculture/adapter/StoryViewWithCommentAdapter.java
@@ -1,9 +1,12 @@
 package tr.edu.boun.cmpe.sculture.adapter;
-
 import android.app.Activity;
-import android.support.v7.widget.LinearLayoutManager;
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,23 +14,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import android.widget.Toast;
 import java.util.ArrayList;
-
-import tr.edu.boun.cmpe.sculture.Constants;
 import tr.edu.boun.cmpe.sculture.R;
 import tr.edu.boun.cmpe.sculture.Utils;
 import tr.edu.boun.cmpe.sculture.models.response.CommentResponse;
 import tr.edu.boun.cmpe.sculture.models.response.FullStoryResponse;
-
 import static tr.edu.boun.cmpe.sculture.BaseApplication.baseApplication;
-
 public class StoryViewWithCommentAdapter extends RecyclerView.Adapter<ViewHolder> {
     private static final int VIEW_TYPE_STORY = 1;
     private static final int VIEW_TYPE_COMMENT_EDIT = 2;
@@ -35,21 +28,15 @@ public class StoryViewWithCommentAdapter extends RecyclerView.Adapter<ViewHolder
     private boolean is_story_added = false;
     private FullStoryResponse story;
     private final ArrayList<CommentResponse> comments = new ArrayList<>();
-
     private Activity mActivity;
-
     class StoryViewHolder extends RecyclerView.ViewHolder {
         private TextView title;
         private TextView content;
         private TextView writer;
         private TextView tags;
         private TextView update;
-        private RecyclerView recyclerView;
-        private StoryImageViewAdapter adapter;
         private long owner_id;
         private long editor_id;
-        private ArrayList<String> media_ids = new ArrayList<>();
-
         public StoryViewHolder(View itemView) {
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.story_title);
@@ -57,13 +44,6 @@ public class StoryViewWithCommentAdapter extends RecyclerView.Adapter<ViewHolder
             writer = (TextView) itemView.findViewById(R.id.story_creation);
             tags = (TextView) itemView.findViewById(R.id.storyTags);
             update = (TextView) itemView.findViewById(R.id.story_update);
-            recyclerView = (RecyclerView) itemView.findViewById(R.id.story_media_recycler);
-
-            LinearLayoutManager llm = new LinearLayoutManager(itemView.getContext());
-            llm.setOrientation(LinearLayoutManager.HORIZONTAL);
-            recyclerView.setLayoutManager(llm);
-            adapter = new StoryImageViewAdapter(null, media_ids, false);
-            recyclerView.setAdapter(adapter);
             writer.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -80,12 +60,9 @@ public class StoryViewWithCommentAdapter extends RecyclerView.Adapter<ViewHolder
             });
         }
     }
-
     class CommentEditViewHolder extends RecyclerView.ViewHolder {
-
         private EditText comment;
         private Button submit;
-
         public CommentEditViewHolder(View itemView) {
             super(itemView);
             comment = (EditText) itemView.findViewById(R.id.comment);
@@ -93,40 +70,12 @@ public class StoryViewWithCommentAdapter extends RecyclerView.Adapter<ViewHolder
             submit.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (comment.getText().toString().equals(""))
-                        return;
-                    JSONObject request = new JSONObject();
-                    try {
-                        request.put("storyId", story.id);
-                        request.put("content", comment.getText().toString());
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    comment.setText("");
-                    Utils.addRequest(Constants.API_COMMENT_NEW, request, new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            CommentResponse commentResponse = new CommentResponse(response);
-                            addComment(commentResponse);
-                        }
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            if (error.networkResponse != null && error.networkResponse.data != null) {
-                                VolleyError e = new VolleyError(new String(error.networkResponse.data));
-                                Log.i("ERROR", e.toString());
-                            } else Log.i("ERROR", error.toString());
-                            //TODO ERROR HANDLING
-                        }
-                    }, null);
-
                     Log.i("CLICK", "Comment submit clicked");
                     //TODO CONNECT API
                 }
             });
         }
     }
-
     class CommentViewHolder extends RecyclerView.ViewHolder {
         private EditText comment;
         private TextView writer;
@@ -135,10 +84,8 @@ public class StoryViewWithCommentAdapter extends RecyclerView.Adapter<ViewHolder
         private TextView save;
         private long comment_id;
         private long owner_id;
-
         private boolean is_on_edit = false;
         private String previous = "";
-
         public CommentViewHolder(View itemView) {
             super(itemView);
             comment = (EditText) itemView.findViewById(R.id.comment);
@@ -146,7 +93,6 @@ public class StoryViewWithCommentAdapter extends RecyclerView.Adapter<ViewHolder
             time = (TextView) itemView.findViewById(R.id.comment_update_date);
             editSave = (TextView) itemView.findViewById(R.id.edit_cancel_text);
             save = (TextView) itemView.findViewById(R.id.save_Text);
-
             save.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -164,7 +110,6 @@ public class StoryViewWithCommentAdapter extends RecyclerView.Adapter<ViewHolder
             editSave.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-
                     if (is_on_edit) {
                         comment.setEnabled(false);
                         editSave.setText(mActivity.getString(R.string.edit));
@@ -181,7 +126,6 @@ public class StoryViewWithCommentAdapter extends RecyclerView.Adapter<ViewHolder
             });
         }
     }
-
     public int getItemViewType(int position) {
         if (position == 0)
             return VIEW_TYPE_STORY;
@@ -190,69 +134,67 @@ public class StoryViewWithCommentAdapter extends RecyclerView.Adapter<ViewHolder
         else
             return VIEW_TYPE_COMMENT;
     }
-
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v;
         switch (viewType) {
             case VIEW_TYPE_STORY:
                 v = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_holder_story_view, parent, false);
                 return new StoryViewHolder(v);
-
             case VIEW_TYPE_COMMENT_EDIT:
                 v = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_holder_comment_edit, parent, false);
                 return new CommentEditViewHolder(v);
-
             default:
                 v = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_holder_comment, parent, false);
                 return new CommentViewHolder(v);
-
         }
-
     }
-
-
     public StoryViewWithCommentAdapter(Activity activity) {
-
         this.mActivity = activity;
     }
-
-
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         switch (position) {
             case 0:
                 StoryViewHolder viewHolder = (StoryViewHolder) holder;
-
                 viewHolder.title.setText(story.title);
                 viewHolder.content.setText(story.content);
                 viewHolder.writer.setText(story.owner.username);
                 viewHolder.owner_id = story.owner.id;
                 viewHolder.update.setText(mActivity.getString(R.string.updated_time, Utils.timestampToPrettyString(story.update_date), story.last_editor.username));
                 viewHolder.editor_id = story.last_editor.id;
-                viewHolder.media_ids.clear();
-                viewHolder.media_ids.addAll(story.media);
-
+                //TODO Clickable tags
                 String tags = "";
 
                 //TODO Clickable tags
+                int[] wordLengths = new int[story.tags.size()];
                 for (int i = 0; i < story.tags.size(); i++) {
-                    tags += story.tags.get(i) + ", ";
+                    if (i == story.tags.size() - 1) {
+                        tags += story.tags.get(i);
+                    } else {
+                        tags += story.tags.get(i) + ", ";
+                    }
+                    wordLengths[i] = story.tags.get(i).length();
                 }
-                viewHolder.tags.setText(tags);
-                viewHolder.adapter.notifyDataSetChanged();
+                SpannableString spannable = new SpannableString(tags);
+                int first = 0;
+                for (int i = 0; i < wordLengths.length; i++) {
+                    spannable.setSpan(new ActivitySpan(story.tags.get(i)), first, first + wordLengths[i], Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    first += wordLengths[i] + 2;
+                }
+                //spannable.setSpan(new ActivitySpan(tags));
+                viewHolder.tags.setText(spannable);
+                viewHolder.tags.setMovementMethod(LinkMovementMethod.getInstance());
                 break;
             case 1:
                 break;
             default:
                 CommentViewHolder commentViewHolder = (CommentViewHolder) holder;
                 CommentResponse commentResponse = comments.get(position - 2);
-
                 commentViewHolder.comment_id = commentResponse.comment_id;
                 commentViewHolder.owner_id = commentResponse.owner_id;
                 commentViewHolder.writer.setText(commentResponse.owner_username);
                 commentViewHolder.comment.setText(commentResponse.content);
                 commentViewHolder.time.setText(Utils.timestampToPrettyString(commentResponse.last_edit_date));
-
                 commentViewHolder.comment.setEnabled(false);
                 if (commentResponse.owner_id == baseApplication.getUSER_ID()) {
                     commentViewHolder.editSave.setVisibility(View.VISIBLE);
@@ -262,8 +204,6 @@ public class StoryViewWithCommentAdapter extends RecyclerView.Adapter<ViewHolder
                 break;
         }
     }
-
-
     @Override
     public int getItemCount() {
         int size = comments.size();
@@ -271,17 +211,26 @@ public class StoryViewWithCommentAdapter extends RecyclerView.Adapter<ViewHolder
             size += 2;
         return size;
     }
-
     public void addStory(FullStoryResponse story) {
         this.story = story;
         is_story_added = true;
         this.notifyItemChanged(0);
         this.notifyItemChanged(1);
-
     }
-
     public void addComment(CommentResponse commentResponse) {
         comments.add(commentResponse);
         this.notifyItemInserted(comments.size() + 2);
+    }
+    public class ActivitySpan extends ClickableSpan {
+        String keyword;
+        public ActivitySpan(String keyword) {
+            super();
+            this.keyword = keyword;
+        }
+        @Override
+        public void onClick(View v) {
+            Context context = v.getContext();
+            Toast.makeText(mActivity.getApplicationContext(), keyword, Toast.LENGTH_SHORT).show();
+        }
     }
 }

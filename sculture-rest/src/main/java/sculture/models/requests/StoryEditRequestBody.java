8c9ad689d952d0229eea655e26babe588509e078
bilalyasar <bilalyasar45@gmail.com>
2015-12-14 01:29:11 +0200

    story edit req body added

---
+++ sculture-rest/src/main/java/sculture/models/requests/StoryEditRequestBody.java
@@ -0,0 +1,51 @@
+package sculture.models.requests;
+
+import java.util.ArrayList;
+
+public class StoryEditRequestBody {
+    private String title;
+    private String content;
+    private long story_id;
+    private ArrayList<String> tags;
+    private ArrayList<String> media;
+
+    public long getStory_id() {
+        return story_id;
+    }
+
+    public void setStory_id(long story_id) {
+        this.story_id = story_id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(ArrayList<String> tags) {
+        this.tags = tags;
+    }
+
+    public ArrayList<String> getMedia() {
+        return media;
+    }
+
+    public void setMedia(ArrayList<String> media) {
+        this.media = media;
+    }
+}

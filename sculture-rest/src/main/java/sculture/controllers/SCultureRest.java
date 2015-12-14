ce33d4961d7f23412ffd2ffc5fe8790f09a671c3
Fatih Doğan <fatih.fd.dogan@gmail.com>
2015-12-15 01:59:04 +0200

    List comment from newest to oldest properly

--- sculture-rest/src/main/java/sculture/controllers/SCultureRest.java
+++ sculture-rest/src/main/java/sculture/controllers/SCultureRest.java
@@ -447,13 +447,10 @@ public class SCultureRest {
 
         List<Comment> comments = commentDao.retrieveByStory(requestBody.getStory_id(), page, size);
         List<CommentResponse> responses = new LinkedList<>();
-        Collections.sort(comments);
-        for (int i = comments.size() - 1; i >= 0; i--) {
-            responses.add(new CommentResponse(comments.get(i), userDao));
-        }
-        /*for (Comment comment : comments) {
+
+        for (Comment comment : comments) {
             responses.add(new CommentResponse(comment, userDao));
-        }*/
+        }
 
 
         CommentListResponse commentListResponse = new CommentListResponse();

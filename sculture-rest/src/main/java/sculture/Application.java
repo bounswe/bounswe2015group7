ee4feb179c753f13cb1faf70b44fd67b7ebe533d
bilalyasar <bilalyasar45@gmail.com>
2016-01-05 01:42:19 +0200

    rest fix

--- sculture-rest/src/main/java/sculture/Application.java
+++ sculture-rest/src/main/java/sculture/Application.java
@@ -1,18 +1,30 @@
 package sculture;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import sculture.lucene.SearchEngine;
 
 import java.io.File;
 
 @SpringBootApplication
-public class Application {
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application extends SpringBootServletInitializer {
 
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         SearchEngine.initialize();
         new File("/image").mkdirs();
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }

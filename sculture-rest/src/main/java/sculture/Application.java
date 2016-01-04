50099bb74da46156b3b1e5e81394eb2f4b0bdf77
Fatih Doğan <fatih.fd.dogan@gmail.com>
2016-01-04 04:54:30 +0200

    [REST] wip

--- sculture-rest/src/main/java/sculture/Application.java
+++ sculture-rest/src/main/java/sculture/Application.java
@@ -4,12 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import sculture.lucene.SearchEngine;
 
+import java.io.File;
+
 @SpringBootApplication
 public class Application {
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         SearchEngine.initialize();
+        File image_root = new File("/image");
+        if (!image_root.exists())
+            image_root.mkdirs();
     }
 }
